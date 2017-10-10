336f831b509588074f746ee0f9b427422f45cd5e
mirimCZ <l.jecha@seznam.cz>
2017-10-10 13:33:19 +0200

    core front end structure

---
+++ src/DetailPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailPage = () => (
+  <div>
+
+  </div>
+)
+
+export default DetailPage
--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import ApolloClient, { createBatchingNetworkInterface, ApolloProvider } from 'react-apollo'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import ListPage from './ListPage'
+import DetailPage from './DetailPage'
+
+const networkInterface = createBatchingNetworkInterface({
+  uri: 'http://localhost:3000/graphql',
+  batchInterval: 30,
+})
+
+const apolloClient = new ApolloClient({
+  networkInterface: networkInterface,
+  shouldBatch: true,
+})
 
 const Root = () => (
-  <div>
-    This is app
-  </div>
+  <ApolloProvider client={apolloClient}>
+    <BrowserRouter>
+      <div id="app">
+        <div id="head">
+          <ul>
+            <li><Link to="/">List</Link></li>
+            <li><Link to="/article/1">Article</Link></li>
+          </ul>
+        </div>
+        <div id="content">
+          <Switch>
+            <Route exact path="/" component={ListPage} />
+            <Route path="/article/:id" component={DetailPage} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  </ApolloProvider>
 )
 
 export default Root
